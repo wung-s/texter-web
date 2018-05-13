4e52e7696440ba83fd26338ada811a3273f22b8c
Wung S <wungmathing@gmail.com>
2018-05-13 16:45:40 +0000

    Merged in redirection (pull request #8)
    :new: Block access to protected pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
 import ConversationContainer from "./containers/ConversationContainer"
 import HomeContainer from "./containers/HomeContainer"
+import { isLoggedIn } from "./helpers/Auth"
 
 const App = () => (
   <Router>
     <div>
-      <Route exact path="/" component={HomeContainer} />
-      <Route path="/conversations" component={ConversationContainer} />
+      <Route
+        exact
+        path="/"
+        render={() => (isLoggedIn() ? <Redirect to="/conversations" /> : <HomeContainer />)}
+      />
+      <Route
+        exact
+        path="/conversations"
+        render={() => (isLoggedIn() ? <ConversationContainer /> : <Redirect to="/" />)}
+      />
     </div>
   </Router>
 )
